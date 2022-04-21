6705dc60decb5cb59037df9426639aa7cc9e5c34
JohnFarrellDev <johnfarrell@fastmail.com>
2022-04-21 12:25:54 +0100

    github action workflow

--- components/index.test.tsx
+++ components/index.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from "@testing-library/react";
-import { Index } from "./index";
+import { render, screen } from '@testing-library/react';
+import { Index } from './index';
 
-describe("Index", () => {
-  it("renders a heading", () => {
+describe('Index', () => {
+  it('renders a heading', () => {
     render(<Index />);
 
-    const heading = screen.getByRole("heading", {
+    const heading = screen.getByRole('heading', {
       name: /welcome to next\.js!/i,
     });
 
