6ffd3a4c58b511f9fdeea596e400cb373f68b883
mattedwards-mt <matt.edwards@madetech.com>
2022-04-21 16:04:49 +0100

    Move api request into its own module.

--- pages/api-test.tsx
+++ pages/api-test.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import getForecastData from "../requests/getForecastData";
 
 import ApiTestComponent from "../components/api-test";
 import { AppProps } from "../interfaces/api-data-hourly";
@@ -12,28 +12,7 @@ const ApiTest = ({ data }: AppProps) => {
 export default ApiTest;
 
 export const getServerSideProps = async () => {
-  const { data } = await axios.get(`${process.env.MET_OFFICE_API_BASE_URL}hourly`, {
-    headers: {
-      "X-IBM-Client-Id": `${process.env.MET_OFFICE_API_CLIENT_ID}`,
-      "X-IBM-Client-Secret": `${process.env.MET_OFFICE_API_CLIENT_SECRET}`,
-      accept: "application/json"
-    },
-    params: {
-      "excludeParameterMetadata": true,
-      "includeLocationName": true,
-      "latitude": 50.2113,
-      "longitude": -5.4813
-    }
-  });  
-
+  const { data } = await getForecastData("hourly");
   if (!data) return { notFound: true };
   return { props: { data }};
 }
-
-// Manchester:
-// lat: 53.4808
-// long: 2.2426
-
-// St Ives:
-// lat: 50.2113
-// long: -5.4813
