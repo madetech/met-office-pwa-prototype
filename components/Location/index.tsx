ee48a6a15900c715a8a2793ce665967e33ffa031
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-04-25 19:59:45 +0100

    Add geocode logic for returning formatted address from lat / long

--- components/Location/index.tsx
+++ components/Location/index.tsx
@@ -1,11 +1,26 @@
 import React, { useEffect } from 'react';
+import Geocode from 'react-geocode';
 
 export function Location() {
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(function (position) {
-      console.log('Position object : ', position);
       console.log('Latitude is     : ', position.coords.latitude);
       console.log('Longitude is    : ', position.coords.longitude);
+
+      Geocode.setApiKey(apiKey);
+
+      Geocode.fromLatLng(
+        position.coords.latitude.toString(),
+        position.coords.longitude.toString()
+      ).then(
+        (response) => {
+          const address = response.results[0].formatted_address;
+          console.log('Address is: ', address);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     });
 
     return () => console.log('unmounting...');
