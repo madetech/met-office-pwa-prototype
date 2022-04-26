4d016956e6880e2e73cebf260883153704a3ea2f
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-04-26 11:08:15 +0100

    Amend Location to use server-side calls for Google geocoding

--- components/Location/index.tsx
+++ components/Location/index.tsx
@@ -1,42 +1,29 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import Geocode from 'react-geocode';
 import styles from '../../styles/Location.module.css';
 
-interface Props {
-  googleApiKey: string;
-}
-
-export const LocationComponent = ({ googleApiKey }: Props) => {
+export const Location = () => {
   const [latitude, setLatitude] = useState<number>(999);
   const [longitude, setLongitude] = useState<number>(999);
   const [currentAddress, setCurrentAddress] = useState<string>('');
 
   useEffect(() => {
-    if (latitude === 999) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        console.log('Latitude is     : ', position.coords.latitude);
-        console.log('Longitude is    : ', position.coords.longitude);
+    const getAddress = async () => {
+      if (latitude === 999) {
+        navigator.geolocation.getCurrentPosition(async function (position) {
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
 
-        setLatitude(position.coords.latitude);
-        setLongitude(position.coords.longitude);
+          const address = await axios.get<string>(
+            `/api/get-address?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}`
+          );
 
-        Geocode.setApiKey(googleApiKey);
+          setCurrentAddress(address.data);
+        });
+      }
+    };
 
-        Geocode.fromLatLng(
-          position.coords.latitude.toString(),
-          position.coords.longitude.toString()
-        ).then(
-          (response) => {
-            console.log('Address object', response.results[0]);
-
-            setCurrentAddress(response.results[0].formatted_address);
-          },
-          (error) => {
-            console.log(error);
-          }
-        );
-      });
-    }
+    getAddress();
   }, [latitude]);
 
   if (currentAddress) {
