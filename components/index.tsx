8d3e9e5f23091301bbce51036bf479f2d8780c0a
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-04-26 11:25:24 +0100

    Fix merge conflict

--- components/index.tsx
+++ components/index.tsx
@@ -1,9 +1,13 @@
 import { HourlyData } from '../interfaces/api-data-hourly';
 import { Navigation } from './Navigation';
+<<<<<<< HEAD
 import { useRouter } from 'next/router';
 import { Login } from './login';
 import { Search } from './Search';
 import { Forecast } from './Forecast';
+=======
+import { Location } from './Location';
+>>>>>>> 3c97751... Amend Index to add Location component to page
 import styles from '../styles/Index.module.css';
 
 interface IndexProps {
@@ -20,8 +24,28 @@ export const Index = ({ hasReadPermission, data }: IndexProps) => {
   return (
     <main className={styles.pageContainer}>
       <Navigation />
+<<<<<<< HEAD
       <Search />
       <Forecast data={data} />
     </main>
+=======
+
+      <Location />
+
+      <h1>{placeName}</h1>
+      <h2>
+        Lat: {lat} / Long: {long}
+      </h2>
+      {forecasts.map((forecast, index: number) => {
+        return (
+          <p key={index}>
+            {forecast.time} | Temp: {forecast.screenTemperature} | Precip.
+            probability {forecast.probOfPrecipitation} | Weather code:{' '}
+            {forecast.significantWeatherCode}
+          </p>
+        );
+      })}
+    </div>
+>>>>>>> 3c97751... Amend Index to add Location component to page
   );
 };
