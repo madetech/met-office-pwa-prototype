d4be1f09328081e58808a92dc35b1008c967bd81
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-04-26 12:10:19 +0100

    Amend initial check to avoid magic numbers

--- components/Location/index.tsx
+++ components/Location/index.tsx
@@ -3,13 +3,13 @@ import React, { useState, useEffect } from 'react';
 import styles from '../../styles/Location.module.css';
 
 export const Location = () => {
-  const [latitude, setLatitude] = useState<number>(999);
-  const [longitude, setLongitude] = useState<number>(999);
+  const [latitude, setLatitude] = useState<number | null>(null);
+  const [longitude, setLongitude] = useState<number | null>(null);
   const [currentAddress, setCurrentAddress] = useState<string>('');
 
   useEffect(() => {
     const getAddress = async () => {
-      if (latitude === 999) {
+      if (latitude === null) {
         navigator.geolocation.getCurrentPosition(async function (position) {
           setLatitude(position.coords.latitude);
           setLongitude(position.coords.longitude);
