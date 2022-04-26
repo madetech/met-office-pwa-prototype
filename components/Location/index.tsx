16af50c35fb4fa9c79e25eb06345361452c77760
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-04-26 09:07:56 +0100

    Amend component to pass Google API key as prop

--- components/Location/index.tsx
+++ components/Location/index.tsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import Geocode from 'react-geocode';
 import styles from '../../styles/Location.module.css';
 
-export const LocationComponent = () => {
+interface Props {
+  googleApiKey: string;
+}
+
+export const LocationComponent = ({ googleApiKey }: Props) => {
   const [latitude, setLatitude] = useState<number>(999);
   const [longitude, setLongitude] = useState<number>(999);
+  const [currentAddress, setCurrentAddress] = useState<string>('');
 
   useEffect(() => {
     if (latitude === 999) {
@@ -13,17 +19,31 @@ export const LocationComponent = () => {
 
         setLatitude(position.coords.latitude);
         setLongitude(position.coords.longitude);
+
+        Geocode.setApiKey(googleApiKey);
+
+        Geocode.fromLatLng(
+          position.coords.latitude.toString(),
+          position.coords.longitude.toString()
+        ).then(
+          (response) => {
+            setCurrentAddress(response.results[0].formatted_address);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
       });
     }
   }, [latitude]);
 
-  if (longitude === 999) {
-    return <div className={styles.notFound} />;
+  if (currentAddress) {
+    return (
+      <div className={styles.address}>
+        {currentAddress} (lat: {latitude}/lng: {longitude})
+      </div>
+    );
   }
 
-  return (
-    <div className={styles.address}>
-      Current latitude: {latitude} / longitude: {longitude}
-    </div>
-  );
+  return <div className={styles.notFound} />;
 };
