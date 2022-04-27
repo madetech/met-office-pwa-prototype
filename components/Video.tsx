c6e51c5820c104dec34200b6345c87a368f5d7f2
JohnFarrellDev <johnfarrell@fastmail.com>
2022-04-27 12:36:09 +0100

    remove commented code

--- components/Video.tsx
+++ components/Video.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { YoutubePlaylistApiResponse } from '../interfaces/youtube-api';
 import styles from '../styles/Video.module.css';
 import { DraggableTile } from './DraggableTile';
-// import ReactPlayer from 'react-player/youtube';
 import dynamic from 'next/dynamic';
 
 interface VideoProps {
