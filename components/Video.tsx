cd25b85ba402c6da183328c0ce9fc2f28919ea52
JohnFarrellDev <johnfarrell@fastmail.com>
2022-04-27 12:33:13 +0100

    init react player for youtube

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { YoutubePlaylistApiResponse } from '../interfaces/youtube-api';
 import styles from '../styles/Video.module.css';
 import { DraggableTile } from './DraggableTile';
+import ReactPlayer from 'react-player/youtube';
 
 interface VideoProps {
   videoData: YoutubePlaylistApiResponse;
 }
 
 export const Video = ({ videoData }: VideoProps) => {
+  const [hasWindow, setHasWindow] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setHasWindow(true);
+    }
+  }, []);
+
   return (
     <DraggableTile>
       <h2>UK Video Forecast</h2>
       <div className={styles.videoContainer}>
-        <iframe
-          src={`https://www.youtube.com/embed/${videoData.items[0].snippet.resourceId.videoId}`}
-          allow="autoplay; encrypted-media; "
-          allowFullScreen
-          title="Tuesday afternoon forecast 26/04/2022"
-        />
+        {hasWindow && (
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${videoData.items[0].snippet.resourceId.videoId}`}
+          />
+        )}
       </div>
     </DraggableTile>
   );
