491ebd6a06eec9c6264088af4e4281be7276150d
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-04-27 08:30:35 +0100

    Add server side api call for getting the weather

--- pages/api/get-weather-forecast.ts
+++ pages/api/get-weather-forecast.ts
@@ -6,9 +6,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const response = await axios.get<HourlyData>(
-    `${process.env.MET_OFFICE_API_BASE_URL}${req.query.frequency}`,
-    {
+  const urlString = `${process.env.MET_OFFICE_API_BASE_URL}${req.query.frequency}`;
+
+  const response = await axios
+    .get<HourlyData>(urlString, {
       headers: {
         'X-IBM-Client-Id': `${process.env.MET_OFFICE_API_CLIENT_ID}`,
         'X-IBM-Client-Secret': `${process.env.MET_OFFICE_API_CLIENT_SECRET}`,
@@ -20,8 +21,25 @@ export default async function handler(
         latitude: req.query.latitude,
         longitude: req.query.longitude,
       },
-    }
-  );
+    })
+    .catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    });
 
-  res.status(200).json(response);
+  res.status(200).json(response.data);
 }
