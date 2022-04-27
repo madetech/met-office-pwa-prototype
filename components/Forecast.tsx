4661383283f35d7ccd5e5f24d733ba3ce3ce86b6
mattedwards-mt <matt.edwards@madetech.com>
2022-04-27 14:22:25 +0100

    Add refresh button to Forecast component that calls latest data for location.

--- components/Forecast.tsx
+++ components/Forecast.tsx
@@ -1,4 +1,7 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { HourlyData } from '../interfaces/api-data-hourly';
+import getForecastData from '../requests/getForecastData';
 import styles from '../styles/Forecast.module.css';
 import { Timeslot } from './Timeslot';
 
@@ -21,15 +24,25 @@ interface ForecastProps {
 }
 
 export const Forecast = ({ data }: ForecastProps) => {
-  const long = formatLongitude(data.features[0].geometry.coordinates[0]);
-  const lat = formatLatitude(data.features[0].geometry.coordinates[1]);
-  const placeName = data.features[0].properties.location.name;
+  const [forecastData, setForecastData] = useState(data);
+
+  const coords = forecastData.features[0].geometry.coordinates;
+  const long = formatLongitude(coords[0]);
+  const lat = formatLatitude(coords[1]);
+  const placeName = forecastData.features[0].properties.location.name;
 
   const currenTimeMinusOneHour = new Date(Date.now());
   currenTimeMinusOneHour.setMinutes(1);
   currenTimeMinusOneHour.setHours(currenTimeMinusOneHour.getHours() - 1);
 
-  const forecasts = data.features[0].properties.timeSeries
+  const handleRefresh = async () => {
+    const res = await axios.get<HourlyData>(
+      `/api/get-weather-forecast?frequency=hourly&latitude=${coords[1]}&longitude=${coords[0]}`
+    );
+    setForecastData(res.data);
+  };
+
+  const forecasts = forecastData.features[0].properties.timeSeries
     .filter((forecast) => {
       if (Number(new Date(forecast.time)) < Number(currenTimeMinusOneHour)) {
         return false;
@@ -40,13 +53,17 @@ export const Forecast = ({ data }: ForecastProps) => {
 
   return (
     <section className="tile">
-      <article className={styles.locationData}>
-        <h2 className={styles.locationName}>{placeName}</h2>
-        <div className={styles.locationPosition}>
-          <span>Lat: {lat}</span>
-          <span>Long: {long}</span>
-        </div>
-      </article>
+      <section className={styles.heading}>
+        <article className={styles.locationData}>
+          <h2 className={styles.locationName}>{placeName}</h2>
+          <span className={styles.locationPosition}>
+            Lat: {lat} / Long: {long}
+          </span>
+        </article>
+        <button className={styles.refresh} onClick={handleRefresh}>
+          Refresh
+        </button>
+      </section>
 
       <section className={styles.timeslots}>
         {forecasts.map((forecast) => {
