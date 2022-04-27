924748ab93b0b28eb2acfd84e066b9cea3bbe316
JohnFarrellDev <johnfarrell@fastmail.com>
2022-04-27 11:58:38 +0100

    embed video based on id from youtube api

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { YoutubePlaylistApiResponse } from '../interfaces/youtube-api';
 import styles from '../styles/Video.module.css';
-export const Video = () => {
+
+interface VideoProps {
+  videoData: YoutubePlaylistApiResponse;
+}
+
+export const Video = ({ videoData }: VideoProps) => {
   return (
     <section className="tile">
       <h2>UK Video Forecast</h2>
       <div className={styles.videoContainer}>
         <iframe
-          src="https://www.youtube.com/embed/Ho7ZNiYP9MU"
+          src={`https://www.youtube.com/embed/${videoData.items[0].snippet.resourceId.videoId}`}
           allow="autoplay; encrypted-media; "
           allowFullScreen
           title="Tuesday afternoon forecast 26/04/2022"
--- requests/getLatestVideoData.ts
+++ requests/getLatestVideoData.ts
@@ -1,16 +1,12 @@
 import axios from 'axios';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { YoutubePlaylistApiResponse } from '../../interfaces/youtube-api';
+import { YoutubePlaylistApiResponse } from '../interfaces/youtube-api';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export const getLatestVideoData = async () => {
   const uploadPlaylistId = 'UU40Tw2tFuMzK305mi7nj8rg';
 
   const urlString = `https://www.googleapis.com/youtube/v3/playlistItems?key=${process.env.YOUTUBE_API_KEY}&playlistId=${uploadPlaylistId}&part=snippet&maxResults=1`;
 
   const response = await axios.get<YoutubePlaylistApiResponse>(urlString);
 
-  res.status(200).json(response.data);
-}
+  return response;
+};
