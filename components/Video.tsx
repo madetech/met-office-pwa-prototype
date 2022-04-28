21697264385e5c40192dfb690357fba81a17ae0c
JohnFarrellDev <johnfarrell@fastmail.com>
2022-04-28 15:05:19 +0100

    massive hack to fullsize video on play

--- components/Video.tsx
+++ components/Video.tsx
@@ -3,7 +3,7 @@ import { YoutubePlaylistApiResponse } from '../interfaces/youtube-api';
 import styles from '../styles/Video.module.css';
 import { DraggableTile } from './DraggableTile';
 import dynamic from 'next/dynamic';
-
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 interface VideoProps {
   videoData: YoutubePlaylistApiResponse;
 }
@@ -14,10 +14,33 @@ export const Video = ({ videoData }: VideoProps) => {
   });
 
   const onPlay = () => {
-    if (document) {
-      const video = document.querySelector('iframe') as HTMLIFrameElement;
-      video.requestFullscreen();
-    }
+    const videoElement = document.querySelector('iframe') as HTMLIFrameElement;
+    const closeButtonElement = document.querySelector(
+      '#close-video-icon'
+    ) as SVGElement;
+
+    videoElement.style.position = 'absolute';
+    videoElement.style.top = '0px';
+    videoElement.style.left = '0px';
+    videoElement.style.bottom = '0px';
+    videoElement.style.right = '0px';
+    videoElement.style.border = 'none';
+    videoElement.style.margin = '0';
+    videoElement.style.padding = '0';
+    videoElement.style.overflow = 'hidden';
+    videoElement.style.zIndex = '100';
+
+    closeButtonElement.style.display = 'inline';
+  };
+
+  const onPause = () => {
+    const videoElement = document.querySelector('iframe') as HTMLIFrameElement;
+    const closeButtonElement = document.querySelector(
+      '#close-video-icon'
+    ) as SVGElement;
+
+    videoElement.removeAttribute('style');
+    closeButtonElement.style.display = 'none';
   };
 
   return (
@@ -28,8 +51,24 @@ export const Video = ({ videoData }: VideoProps) => {
           url={`https://www.youtube.com/watch?v=${videoData.items[0].snippet.resourceId.videoId}`}
           controls={true}
           onPlay={onPlay}
+          onPause={onPause}
+          width="100%"
+          height="100%"
         />
       </div>
+      <AiOutlineCloseCircle
+        id="close-video-icon"
+        style={{
+          position: 'absolute',
+          top: '25px',
+          right: '25px',
+          zIndex: '99999999',
+          color: 'white',
+          display: 'none',
+        }}
+        onClick={onPause}
+        size={100}
+      />
     </DraggableTile>
   );
 };
