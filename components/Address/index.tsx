069d6d56a6aa9583df0a364cae105290148efe95
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-05-02 09:51:27 +0100

    Move local storage into its own folder

--- components/Address/index.tsx
+++ components/Address/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ImCompass } from 'react-icons/im';
 import styles from '../../styles/Location.module.css';
-import useStorage from '../../useLocalStorage';
+import useStorage from '../LocalStorage';
 
 export const Address = () => {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
