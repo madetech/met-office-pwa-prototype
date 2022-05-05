5453540ab4b0dfe5bcb861263f1e5ac9d81fc4a6
JohnFarrellDev <johnfarrell@fastmail.com>
2022-05-05 16:53:00 +0100

    apply background color to ihpone status bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -48,6 +48,10 @@ const Header = () => {
       <meta name="msapplication-TileColor" content="#2B5797" />
       <meta name="msapplication-tap-highlight" content="no" />
       <meta name="theme-color" content="#122a3a" />
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
 
       <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
       <link
