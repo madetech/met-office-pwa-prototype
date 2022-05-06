da7eee2d13cded80e988ba5de464673d1220b2a9
JohnFarrellDev <johnfarrell@fastmail.com>
2022-05-06 13:11:00 +0100

    update search hint text

--- components/Search.tsx
+++ components/Search.tsx
@@ -6,7 +6,7 @@ export const Search = () => {
       <form className={styles.search}>
         <label htmlFor="searchLocation">
           What location would you like to search?
-          <span>eg. Town, City, Postcode.</span>
+          <span>For example, Town or City or Postcode.</span>
         </label>
         <div className={styles.inputs}>
           <input type="text" id="searchLocation" disabled />
