e8f7e3ca4d5e8c7b7a0520216c0d4a9c6bc1775d
JohnFarrellDev <johnfarrell@fastmail.com>
2022-05-06 13:38:22 +0100

    remove unusued import

--- components/WeatherTiles.tsx
+++ components/WeatherTiles.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { DailyForecast } from '../interfaces/api-data-daily';
 import { Forecast } from '../interfaces/api-data-hourly';
 import timeslotStyles from '../styles/Forecast.module.css';
