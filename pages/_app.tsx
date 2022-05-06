420d1bce81b318fb46d14fcd9a4f3409a26c2d9f
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-05-06 13:52:46 +0100

    Restore password prompt - removed in error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import App, { AppContext, AppProps } from 'next/app';
 import '../styles/globals.css';
+import Cookies from 'universal-cookie';
+import { LOG_IN_COOKIE_KEY } from '../constants';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -14,7 +16,16 @@ function MyApp({ Component, pageProps }: AppProps) {
 MyApp.getInitialProps = async (appContext: AppContext) => {
   const appProps = await App.getInitialProps(appContext);
 
-  appProps.pageProps.hasReadPermission = true;
+  const cookies = new Cookies(appContext.ctx.req?.headers.cookie);
+  const password = cookies.get(LOG_IN_COOKIE_KEY) ?? '';
+
+  if (`${process.env.REQUIRE_PASSWORD}` !== 'true') {
+    appProps.pageProps.hasReadPermission = true;
+  }
+
+  if (password === `${process.env.LOGIN_PASSWORD}`) {
+    appProps.pageProps.hasReadPermission = true;
+  }
 
   return { ...appProps };
 };
