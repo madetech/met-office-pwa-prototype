3d348c1a8824ac536a379289931f0c1f5573b0b7
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-05-06 09:14:04 +0100

    Remove password prompt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import App, { AppContext, AppProps } from 'next/app';
 import '../styles/globals.css';
-import Cookies from 'universal-cookie';
-import { LOG_IN_COOKIE_KEY } from '../constants';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -16,16 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 MyApp.getInitialProps = async (appContext: AppContext) => {
   const appProps = await App.getInitialProps(appContext);
 
-  const cookies = new Cookies(appContext.ctx.req?.headers.cookie);
-  const password = cookies.get(LOG_IN_COOKIE_KEY) ?? '';
-
-  if (`${process.env.REQUIRE_PASSWORD}` !== 'true') {
-    appProps.pageProps.hasReadPermission = true;
-  }
-
-  if (password === `${process.env.LOGIN_PASSWORD}`) {
-    appProps.pageProps.hasReadPermission = true;
-  }
+  appProps.pageProps.hasReadPermission = true;
 
   return { ...appProps };
 };
